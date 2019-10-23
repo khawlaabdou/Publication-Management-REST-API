e8ceaa3bdf9a26f13e54f6fa5420fc9379197dba
SaberElghoul <saber.elghoul@etudiant-fst.utm.tn>
2019-10-23 16:21:35 +0100

    Post-test

--- src/test/java/rest/api/post/demo/controllers/PostControllerTest.java
+++ src/test/java/rest/api/post/demo/controllers/PostControllerTest.java
@@ -72,45 +72,43 @@ class PostControllerTest {
 
     @Test
     void testUpdatePost() {
-        // Setup
+
         final String id = "id";
         final String title = "title";
         final String value = "value";
         final Post expectedResult = new Post("id", "value", 0, null);
         when(mockPostService.update("title", "body")).thenReturn(new Post("id", "value", 0, null));
 
-        // Run the test
         final Post result = postControllerUnderTest.updatePost(id, title, value);
 
-        // Verify the results
+
         assertEquals(expectedResult, result);
     }
 
     @Test
     void testDeletePost() {
-        // Setup
+
         final String id = "id";
         final String expectedResult = "result";
 
-        // Run the test
+
         final String result = postControllerUnderTest.deletePost(id);
 
-        // Verify the results
         assertEquals(expectedResult, result);
         verify(mockPostService).deleteById("id");
     }
 
     @Test
     void testGetPostByFewLetters() {
-        // Setup
+
         final String title = "title";
         final List<Post> expectedResult = Arrays.asList();
         when(mockPostService.getAll()).thenReturn(Arrays.asList());
 
-        // Run the test
+
         final List<Post> result = postControllerUnderTest.getPostByFewLetters(title);
 
-        // Verify the results
+
         assertEquals(expectedResult, result);
     }
 }
